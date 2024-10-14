396af15764287ed5db3f948e12a5347a22631d1c
saagor16 <saagor.16@gmail.com>
2024-10-14 22:54:19 +0600

    Class 42   done index

--- javascript/Class 42/index42.js
+++ javascript/Class 42/index42.js
@@ -3,3 +3,11 @@ document
   .addEventListener("click", function() {
     alert("hello");
 });
+
+var myVar =document.querySelector("h1");
+myVar.addEventListener("mouseover",function(){
+  myVar.classList.add("my-style")
+});
+myVar.addEventListener("mouseout",function(){
+  myVar.classList.remove("my-style")
+});
