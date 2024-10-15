5a1200e869de26a64094f28ad765ac1ae48b743c
saagor16 <saagor.16@gmail.com>
2024-10-15 14:24:52 +0600

    Class 47 done index

--- javascript/class 47/index47.js
+++ javascript/class 47/index47.js
@@ -4,11 +4,38 @@
 
 
 console.clear()
-const input = document.querySelector('input');
+const input = document.querySelector('input[name=name');
 input.addEventListener('change',changeHandler)
 function changeHandler(e){
     // console.log(e);
     // console.log(e.type);
-    console.log(e.target)
+    // console.log(e.target);
+    // console.log(e.target.className);
+    // console.log(e.target.id);
+    // console.log(e.target.value);
+
 }
-// 10 mint
+
+
+const programs = document.querySelectorAll("input[name=program");
+console.log(programs);
+Array.from(programs).map((program)=>{
+    program.addEventListener("change",programHandler)
+})
+
+
+function programHandler(e){
+    if(e.target.checked){
+        console.log(e.target.value)
+    }
+    
+}
+
+
+const department= document.querySelector("#department");
+
+department.addEventListener("change",handleDepartment)
+
+function handleDepartment(e){
+    console.log(e.target.value)
+}
