5c1339bb8a07574bc16c1a5332ee78f66b9b2d06
saagor16 <saagor.16@gmail.com>
2024-10-18 15:29:24 +0600

    Class 51 done index

--- javascript/class 51/index51.js
+++ javascript/class 51/index51.js
@@ -1,30 +1,73 @@
-//scroll, resize, toggle
-//load ,unload
+//DOM Event | MouseEvent -<html>,<head>,<meta>,<title>,<br>,<style>,<script>,<iframe>,<param>,<base>,<bdo>
+//1. onCLick
+//2. ondblclick
+//3. onmousedown
+//4. onmouseup
+//5. onmouseenter
+//6. onmouseleave
+//7. onmousemove
+//8. onmouseover
+
+console.clear()
+const div = document.querySelector("div");
+console.log(div);
 
 
-window.addEventListener('load',function(){
-    console.log('load')
-})
+// // 1. onClick
+// div.addEventListener('click', function(e) {
+//     console.log('click');
+//     console.log(e.target);
+//     console.log(e.target.id);
+//     console.log(e.target.className);
+//     console.log(e.target.innerHTML);
+//     console.log(e.target.textContent);
+//     console.log(e.target.innerText);
+// });
 
-window.addEventListener('unload',function(){
-    console.log('unload')
-})
+// // 2. ondblclick
+// div.addEventListener('dblclick', function() {
+//     console.log('dblclick');
+// });
 
-window.addEventListener('scroll',function(){
-    console.log('scroll')
-})
+// // 3. onmousedown
+// div.addEventListener('mousedown', function() {
+//     console.log('mousedown');
+// });
 
-window.addEventListener('resize',function(){
-    console.log('resize');
-    const width = this.window.outerWidth;
-    const height = this.window.outerHeight;
-    console.log(`height :${height},width:${width}`)
-})
+// // 4. onmouseup
+// div.addEventListener('mouseup', function() {
+//     console.log('mouseup');
+// });
+
+// // 5. onmouseenter
+// div.addEventListener('mouseenter', function() {
+//     console.log('mouseenter');
+// });
+
+// // 6. onmouseleave
+// div.addEventListener('mouseleave', function() {
+//     console.log('mouseleave');
+// });
+
+// // 7. onmousemove
+// div.addEventListener('mousemove', function(e) {
+//     // console.log('mousemove');
+//     // console.log("clientX= " = e.clientX + ", clientY= "+e.clientY);
+//     // console.log("offsetX= " = e.offsetX + ", offsetY= "+e.offsetY);
+// });
+
+// // 8. onmouseover
+// div.addEventListener('mouseover', function() {
+//     console.log('mouseover');
+// });
 
 
-const details = document.querySelector("details");
 
-details.addEventListener('toggle',function(e){
-    console.log('toggle')
-    console.log(e.target.open)
-})
+const buttons = document.querySelectorAll('.btn');
+
+// Iterate over each button and add the event listener
+buttons.forEach((button) => {
+    button.addEventListener('click', function() {
+        console.log('clicked');
+    });
+});
