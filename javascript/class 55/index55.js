89513a512d6cb36aa8cfbd6455cae5e7dc469aec
saagor16 <saagor.16@gmail.com>
2024-10-23 18:28:16 +0600

    Class 54 done index

--- javascript/class 55/index55.js
+++ javascript/class 55/index55.js
@@ -4,21 +4,17 @@
 // onpaste
 
 const input = document.querySelector('input')
-const p = document.querySelector('p')
 
 input.addEventListener('copy',function(){
     console.log('you have copied');
-    p.innerText = "you have copied";
 })
 
 input.addEventListener('cut',function(){
     console.log('you have cut');
-    p.innerText = "you have cut";
 })
 
-input.addEventListener('paste',function(){
+input.addEventListener('copy',function(){
     console.log('you have paste');
-    p.innerText = "you have paste";
 })
 
 
