37660044936c76272ac1bbe0bed7a2a51d4070ab
saagor16 <saagor.16@gmail.com>
2024-10-25 23:12:10 +0600

    Class 56 start index t

--- javascript/class 55/index55.js
+++ javascript/class 55/index55.js
@@ -1,3 +1,15 @@
-//DragEvent
 
-
+// ClipboardEvent object
+// oncopy
+// oncut
+// onpaste
+const input = document.querySelector('input')
+input.addEventListener('copy',function(){
+    console.log('you have copied');
+})
+input.addEventListener('cut',function(){
+    console.log('you have cut');
+})
+input.addEventListener('copy',function(){
+    console.log('you have paste');
+})
