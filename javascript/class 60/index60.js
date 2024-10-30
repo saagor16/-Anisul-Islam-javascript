0bcdabdd71ee15a7ca6c8446ef54badbdfd68e7e
saagor16 <saagor.16@gmail.com>
2024-10-30 10:37:06 +0600

    Class 60 done

---
+++ javascript/class 60/index60.js
@@ -0,0 +1,14 @@
+// best practices for javascript coding
+// use camelCase for naming
+// variable declaration on top and initialize
+// avoid unnecessary variables
+// object and array with const
+// = vs
+// access dom less
+
+
+
+
+
+
+
