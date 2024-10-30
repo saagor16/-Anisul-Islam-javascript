67e28b2c1c4309b9ff07e123d8f9dce7e11d2ad1
saagor16 <saagor.16@gmail.com>
2024-10-30 11:44:57 +0600

    Class 61 done

--- javascript/class 60/index60.js
+++ javascript/class 60/index60.js
@@ -1,5 +1,9 @@
 // best practices for javascript coding
 // use camelCase for naming
+// const firstName,lastName,userInfo;
+
+
+
 // variable declaration on top and initialize
 // avoid unnecessary variables
 // object and array with const
---
+++ javascript/class 61/index61.js
@@ -0,0 +1,34 @@
+// Error Heading -> try,catch,finally,throw
+// try....catch handle run time error
+// Error object
+// the finally statement-> execute code
+
+try{
+    // code test
+    alert('hi everyone');
+    alert(x);
+    alert('Bye everyone');
+}catch(err){
+    // handle error
+    alert('Inside catch');
+    console.log(err);
+    console.log(err.name);
+    console.log(err.massage);
+}finally{
+    alert('Bye everyone');
+}
+
+
+
+// alert('hi everyone');
+// alert(x);
+// alert('Bye everyone');
+
+
+
+
+
+
+
+
+
