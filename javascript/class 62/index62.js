6555cca3e0236488c886678d8cb9f9bc34ea7f4a
saagor16 <saagor.16@gmail.com>
2024-11-01 09:53:52 +0600

    Class 62 done

--- javascript/class 62/index62.js
+++ javascript/class 62/index62.js
@@ -1,5 +1,25 @@
+// how to handle error in JavaScript (part-2) throw keyword
+// Error Handling - try, catch,finally
+// the throe statement -> create custom error
 
+document.querySelector('#check').addEventListener('click',function(){
+    var num = document.querySelector("#numTextfield").value;
 
+    console.log(num)
+    try{
+        if(num<5)
+        {
+            throw "input is to low"
+        }
+        else if(num>10)
+        {
+            throw "input is to high"
+        }
+    }catch(err){
+        console.log(err)
+    }
+
+})
 
 
 
