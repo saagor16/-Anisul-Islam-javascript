017ee2f1f666f875dae5ddd11eb38414a1ceeb60
saagor16 <saagor.16@gmail.com>
2024-11-09 12:50:53 +0600

    Class 72 done all

---
+++ javascript/class 72/index72.js
@@ -0,0 +1,17 @@
+// Arrow function (part-1)
+
+function add(a, b) {
+  return a + b;
+}
+console.log(add(3, 4)); // Output: 7
+
+const add = (a, b) => {
+  return a + b;
+};
+console.log(add(3, 4)); // Output: 7
+
+const multiply = (a, b) => a * b;
+console.log(multiply(3, 4)); // Output: 12
+
+
+
